4cdd645ca833ab9ade2876d9436a4efa47a42a7d
Kaskaya <recep.tkaskaya@gmail.com>
2024-08-05 18:07:39 +0300

    fix ui and css

--- src/components/SkillTree.jsx
+++ src/components/SkillTree.jsx
@@ -3,30 +3,32 @@ import { motion, useAnimation } from "framer-motion";
 import "./SkillTree.css";
 
 const explanations = {
-  "b1-s1": "Explanation for Offensive Skill 1",
-  "b1-s2": "Explanation for Offensive Skill 2",
-  "b1-s3": "Explanation for Offensive Skill 3",
-  "b1-s4": "Explanation for Offensive Skill 4",
-  "b1-s5": "Explanation for Offensive Skill 5",
-  "b1-s6": "Explanation for Offensive Skill 6",
-  "b1-s7": "Explanation for Offensive Skill 7",
-  "b1-s8": "Explanation for Offensive Skill 8",
-  "b2-s1": "Explanation for Defensive Skill 1",
-  "b2-s2": "Explanation for Defensive Skill 2",
-  "b2-s3": "Explanation for Defensive Skill 3",
-  "b2-s4": "Explanation for Defensive Skill 4",
-  "b2-s5": "Explanation for Defensive Skill 5",
-  "b2-s6": "Explanation for Defensive Skill 6",
-  "b2-s7": "Explanation for Defensive Skill 7",
-  "b2-s8": "Explanation for Defensive Skill 8",
-  "b3-s1": "Explanation for Active Skill 1",
-  "b3-s2": "Explanation for Active Skill 2",
-  "b3-s3": "Explanation for Active Skill 3",
-  "b3-s4": "Explanation for Active Skill 4",
-  "b3-s5": "Explanation for Active Skill 5",
-  "b3-s6": "Explanation for Active Skill 6",
-  "b3-s7": "Explanation for Active Skill 7",
-  "b3-s8": "Explanation for Active Skill 8",
+  "b1-s1": "+%20 More damage when using blunt weapons",
+  "b1-s2": "Your hits make enemy bleed",
+  "b1-s3": "Your hits make enemy froze",
+  "b1-s4": "Gain +%25 Attack Power",
+  "b1-s5": "A perfect parry makes your next attack a critical attack",
+  "b1-s6": "Set your weapon on fire and make enemies burn",
+  "b1-s7":
+    "Hitting enemies over time raises attack power by +%3. Stacks up to 5 times",
+  "b1-s8":
+    "Activate the Meat Grinder. Gain +%20 Attack Power and +%20 Ability Power. Your Attacks restores Hp",
+  "b2-s1": "Defensive Skill 1",
+  "b2-s2": "Defensive Skill 2",
+  "b2-s3": "Defensive Skill 3",
+  "b2-s4": "Defensive Skill 4",
+  "b2-s5": "Defensive Skill 5",
+  "b2-s6": "Defensive Skill 6",
+  "b2-s7": "Defensive Skill 7",
+  "b2-s8": "Defensive Skill 8",
+  "b3-s1": "Active Skill 1",
+  "b3-s2": "Active Skill 2",
+  "b3-s3": "Active Skill 3",
+  "b3-s4": "Active Skill 4",
+  "b3-s5": "Active Skill 5",
+  "b3-s6": "Active Skill 6",
+  "b3-s7": "Active Skill 7",
+  "b3-s8": "Active Skill 8",
 };
 
 const createSkillsData = (useAnimation) => [
@@ -69,7 +71,6 @@ const SkillTree = () => {
   const [skillPoints, setSkillPoints] = useState(20);
   const [skills, setSkills] = useState(createSkillsData(useAnimation));
   const [hoveredSkill, setHoveredSkill] = useState(null);
-  const [lockedSkills, setLockedSkills] = useState({});
 
   const handleSkillClick = (branchIndex, skillIndex) => {
     const newSkills = [...skills];
@@ -138,24 +139,85 @@ const SkillTree = () => {
       <div key={`branch-${branchIndex}-row-${rowIndex}`} className="row">
         {row.map((skillIndex) => (
           <div key={branch.skills[skillIndex].id} className="skill-wrapper">
-            {rowIndex > 0 && row.length === 2 && (
+            {rowIndex === 1 && row.length === 2 && (
               <>
-                {skillIndex === 1 || skillIndex === 6 || skillIndex === 4 ? (
+                {skillIndex === 1 && (
                   <motion.div
-                    className={`connector-left ${
-                      branch.skills[skillIndex - 1].selected ? "" : ""
+                    className={`connector-straight ${
+                      branch.skills[0].selected &&
+                      branch.skills[skillIndex].selected
+                        ? "active"
+                        : ""
                     }`}
                     initial={{}}
-                    animate={{}}
+                    animate={{
+                      borderColor:
+                        branch.skills[0].selected &&
+                        branch.skills[skillIndex].selected
+                          ? "yellow"
+                          : "gray",
+                    }}
                     transition={{ duration: 0.5 }}
                   />
-                ) : (
+                )}
+                {skillIndex === 2 && (
                   <motion.div
                     className={`connector-right ${
-                      branch.skills[skillIndex - 1].selected ? "" : ""
+                      branch.skills[0].selected &&
+                      branch.skills[skillIndex].selected
+                        ? "active"
+                        : ""
                     }`}
                     initial={{}}
-                    animate={{}}
+                    animate={{
+                      borderColor:
+                        branch.skills[0].selected &&
+                        branch.skills[skillIndex].selected
+                          ? "yellow"
+                          : "gray",
+                    }}
+                    transition={{ duration: 0.5 }}
+                  />
+                )}
+              </>
+            )}
+            {rowIndex > 0 && row.length === 2 && (
+              <>
+                {skillIndex === row[0] && (
+                  <motion.div
+                    className={`connector-left ${
+                      branch.skills[skillIndex].selected &&
+                      branch.skills[row[0]].selected
+                        ? "active"
+                        : ""
+                    }`}
+                    initial={{}}
+                    animate={{
+                      borderColor:
+                        branch.skills[skillIndex].selected &&
+                        branch.skills[row[0]].selected
+                          ? "yellow"
+                          : "gray",
+                    }}
+                    transition={{ duration: 0.5 }}
+                  />
+                )}
+                {skillIndex === row[1] && (
+                  <motion.div
+                    className={`connector-right ${
+                      branch.skills[skillIndex].selected &&
+                      branch.skills[row[1]].selected
+                        ? "active"
+                        : ""
+                    }`}
+                    initial={{}}
+                    animate={{
+                      borderColor:
+                        branch.skills[skillIndex].selected &&
+                        branch.skills[row[1]].selected
+                          ? "yellow"
+                          : "gray",
+                    }}
                     transition={{ duration: 0.5 }}
                   />
                 )}
@@ -166,55 +228,99 @@ const SkillTree = () => {
                 {skillIndex === 3 && (
                   <>
                     <motion.div
-                      className={`connector-left ${
-                        branch.skills[1].selected ? "active" : ""
+                      className={`connector-right ${
+                        branch.skills[1].selected &&
+                        branch.skills[skillIndex].selected
+                          ? "active"
+                          : ""
                       }`}
                       initial={{}}
-                      animate={{}}
+                      animate={{
+                        borderColor:
+                          branch.skills[1].selected &&
+                          branch.skills[skillIndex].selected
+                            ? "yellow"
+                            : "gray",
+                      }}
                       transition={{ duration: 0.5 }}
                     />
                     <motion.div
-                      className={`connector-right ${
-                        branch.skills[2].selected ? "active" : ""
+                      className={`connector-left ${
+                        branch.skills[2].selected &&
+                        branch.skills[skillIndex].selected
+                          ? "active"
+                          : ""
                       }`}
                       initial={{}}
-                      animate={{}}
+                      animate={{
+                        borderColor:
+                          branch.skills[2].selected &&
+                          branch.skills[skillIndex].selected
+                            ? "yellow"
+                            : "gray",
+                      }}
                       transition={{ duration: 0.5 }}
                     />
                   </>
                 )}
-
                 {skillIndex === 6 && (
                   <>
                     <motion.div
-                      className={`connector-left ${
-                        branch.skills[5].selected ? "active" : ""
+                      className={`connector-right ${
+                        branch.skills[4].selected &&
+                        branch.skills[skillIndex].selected
+                          ? "active"
+                          : ""
                       }`}
                       initial={{}}
-                      animate={{}}
+                      animate={{
+                        borderColor:
+                          branch.skills[4].selected &&
+                          branch.skills[skillIndex].selected
+                            ? "yellow"
+                            : "gray",
+                      }}
                       transition={{ duration: 0.5 }}
                     />
                     <motion.div
-                      className={`connector-right ${
-                        branch.skills[5].selected ? "active" : ""
+                      className={`connector-left ${
+                        branch.skills[5].selected &&
+                        branch.skills[skillIndex].selected
+                          ? "active"
+                          : ""
                       }`}
                       initial={{}}
-                      animate={{}}
+                      animate={{
+                        borderColor:
+                          branch.skills[5].selected &&
+                          branch.skills[skillIndex].selected
+                            ? "yellow"
+                            : "gray",
+                      }}
                       transition={{ duration: 0.5 }}
                     />
                   </>
                 )}
-                {skillIndex === 7 && (
-                  <motion.div
-                    className={`connector-straight ${
-                      branch.skills[6].selected ? "active" : ""
-                    }`}
-                    initial={{}}
-                    animate={{}}
-                    transition={{ duration: 0.5 }}
-                  />
-                )}
               </>
+            )}
+            {rowIndex > 0 && row.length === 1 && (
+              <motion.div
+                className={`connector-straight ${
+                  branch.skills[rows[rowIndex - 1][0]].selected &&
+                  branch.skills[skillIndex].selected
+                    ? "active"
+                    : ""
+                }`}
+                initial={{}}
+                animate={{
+                  borderColor:
+                    branch.skills[rows[rowIndex - 1][0]].selected &&
+                    branch.skills[skillIndex].selected
+                      ? "yellow"
+                      : "gray",
+                }}
+                transition={{ duration: 0.5 }}
+              />
             )}
             <div
               className="explanation"
@@ -251,14 +357,12 @@ const SkillTree = () => {
       <h1>Skill Tree</h1>
       <p>Skill Points: {skillPoints}</p>
       <div className="branches">
-        {skills
-          .map((branch, index) => (
-            <div key={branch.name} className="branch">
-              <h2>{branch.name}</h2>
-              {renderSkills(branch, index)}
-            </div>
-          ))
-          .reverse()}
+        {skills.map((branch, index) => (
+          <div key={branch.name} className="branch">
+            <h2>{branch.name}</h2>
+            {renderSkills(branch, index)}
+          </div>
+        ))}
       </div>
     </div>
   );
